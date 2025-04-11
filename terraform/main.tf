b87462a093ebee930223e5df52862a1d4d57da34
Angelo <ribeiro.angelo@gmail.com>
2025-04-10 23:03:36 -0400

    Fix port configuration for Corretto 17 on Elastic Beanstalk

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,15 +43,16 @@ resource "aws_elastic_beanstalk_environment" "securelend_auth_env" {
   application         = aws_elastic_beanstalk_application.securelend_auth.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.5.0 running Corretto 17"
   version_label       = aws_elastic_beanstalk_application_version.v1.name
-  
+
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t3.micro"
   }
 
+  # Set Spring Boot server.port via environment variable
   setting {
-    namespace = "aws:elasticbeanstalk:container:java"
+    namespace = "aws:elasticbeanstalk:application:environment"
     name      = "SERVER_PORT"
     value     = "5000"
   }
@@ -74,10 +75,17 @@ resource "aws_elastic_beanstalk_environment" "securelend_auth_env" {
     value     = "LoadBalanced"
   }
 
+  # Configure load balancer listener (port 80 -> 5000)
   setting {
     namespace = "aws:elbv2:listener:80"
     name      = "ListenerEnabled"
     value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "Timeout"
+    value     = "1800"  # Increase timeout for initial deploy
   }
 
   setting {
